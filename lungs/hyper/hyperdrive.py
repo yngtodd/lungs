e486f62278ad7dc0dbe95defe8a584a4ff2e25c4
yngtodd <young.todd.mk@gmail.com>
2018-05-20 14:23:19 -0400

    Hyperparameter optimization tests are go

--- lungs/hyper/hyperdrive.py
+++ lungs/hyper/hyperdrive.py
@@ -3,13 +3,15 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 
-from lungs.parser import parse_args
+from lungs.hyper.parser import parse_args
 from lungs.data.loaders import XRayLoaders
 from lungs.models.lungXnet import LungXnet
 
 import time
 from lungs.utils.logger import print_progress
 from lungs.meters import AverageMeter, mAPMeter
+
+from skopt import forest_minimize
 
 
 def train(epoch, train_loader, optimizer, criterion, model, args):
@@ -21,8 +23,6 @@ def train(epoch, train_loader, optimizer, criterion, model, args):
     model.train()
     end = time.time()
     for batch_idx, (data, target) in enumerate(train_loader):
-        load_time.update(time.time() - end)
-
         bs, n_crops, c, h, w = data.size()
         data = data.view(-1, c, h, w).cuda()
         
@@ -47,7 +47,6 @@ def train(epoch, train_loader, optimizer, criterion, model, args):
 
 def validate(epoch, val_loader, criterion, model, args):
     """"""
-    load_time = AverageMeter(name='loading_time')
     batch_time = AverageMeter(name='batch_time')
     loss_meter = AverageMeter(name='losses')
     mapmeter = mAPMeter()
@@ -55,8 +54,6 @@ def validate(epoch, val_loader, criterion, model, args):
     model.eval()
     end = time.time()
     for batch_idx, (data, target) in enumerate(val_loader):
-        load_time.update(time.time() - end)
-
         bs, n_crops, c, h, w = data.size()
         data = data.view(-1, c, h, w).cuda()
         
@@ -83,20 +80,34 @@ def objective(hyperparams):
     train_loss = AverageMeter(name='train_loss')
     val_loss = AverageMeter(name='val_losses')
 
-    model = LungXnet()
+    growth_rate, block0, block1, block2, block3, bn_size = hyperparams
+    growth_rate = int(growth_rate)
+    block0 = int(block0)
+    block1 = int(block1)
+    block2 = int(block2)
+    block3 = int(block3)
+    bn_size = int(bn_size)
+
+    block = (block0, block1, block2, block3)
+
+    model = LungXnet(growth_rate=growth_rate, block_config=block, bn_size=bn_size)
     if args.cuda and torch.cuda.device_count() > 1:
         model = nn.DataParallel(model)
         model.cuda()
+    model.cuda()
+
+    global optimizer
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     for epoch in range(1, args.num_epochs+1):
         trainloss = train(epoch, train_loader, optimizer, criterion, model, args)
         validloss = validate(epoch, val_loader, criterion, model, args)
         train_loss.update(trainloss)
-        epoch_loss.update(validloss)
+        val_loss.update(validloss)
 
-    train_loss.save(path=args.logspath)
-    val_loss.save(path=args.logspath)
-    return epoch_loss.avg
+    #train_loss.save(path=args.logspath)
+    #val_loss.save(path=args.logspath)
+    return val_loss.avg
    
  
 def main():
@@ -114,15 +125,13 @@ def main():
     train_loader = loaders.train_loader(imagetxt=args.traintxt)
     val_loader = loaders.val_loader(imagetxt=args.valtxt)
    
-    global optimizer 
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
-    
     global criterion 
     criterion = nn.BCELoss(size_average=True)
     if args.cuda:
         criterion.cuda()
 
-    hparams = [(,)]
+    space = [(16,32), (2,6), (2,6), (2,6), (2,6), (1,4)]
+    res_rf = forest_minimize(objective, space, n_calls=15, random_state=0, verbose=True)
 
 
 if __name__=="__main__":
